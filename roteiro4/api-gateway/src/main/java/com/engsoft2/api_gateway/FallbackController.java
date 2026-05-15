bc5f3c344905a268787ca3706949f64b0b44271a
Júlio Machado <julioapm@gmail.com>
2026-05-15 15:17:50 -0300

    atualizacao roteiro4

--- roteiro4/api-gateway/src/main/java/com/engsoft2/api_gateway/FallbackController.java
+++ roteiro4/api-gateway/src/main/java/com/engsoft2/api_gateway/FallbackController.java
@@ -2,18 +2,16 @@ package com.engsoft2.api_gateway;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
-    @GetMapping("path")
+    @GetMapping("/currency-conversion")
     public ResponseEntity<String> currencyConversionFallback() {
         return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .internalServerError()
             .body("Serviços temporariamente indisponíveis. Tente novamente.");
             //Implementação mais adequada seria retornar dados em cache de
             //uma requisição anterior com sucesso
