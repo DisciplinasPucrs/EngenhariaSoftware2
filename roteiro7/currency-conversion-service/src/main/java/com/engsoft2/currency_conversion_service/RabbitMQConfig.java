1ac871db07a6dce971d904a3bc1615c9ea4311f5
Júlio Machado <julioapm@gmail.com>
2026-06-05 14:55:20 -0300

    atualizacao roteiro7

--- roteiro7/currency-conversion-service/src/main/java/com/engsoft2/currency_conversion_service/RabbitMQConfig.java
+++ roteiro7/currency-conversion-service/src/main/java/com/engsoft2/currency_conversion_service/RabbitMQConfig.java
@@ -3,7 +3,7 @@ package com.engsoft2.currency_conversion_service;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +19,7 @@ public static final String FANOUTEXCHANGENAME = "conversions.v1.conversion-reque
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 
     @Bean
