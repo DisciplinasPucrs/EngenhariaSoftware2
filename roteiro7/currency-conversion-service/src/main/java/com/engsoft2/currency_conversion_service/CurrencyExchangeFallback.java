62520c11bf199f94509e542df036a4b666373449
Júlio Machado <julioapm@gmail.com>
2026-06-05 13:45:51 -0300

    roteiro7

---
+++ roteiro7/currency-conversion-service/src/main/java/com/engsoft2/currency_conversion_service/CurrencyExchangeFallback.java
@@ -0,0 +1,14 @@
+package com.engsoft2.currency_conversion_service;
+
+import java.math.BigDecimal;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyExchangeFallback implements CurrencyExchangeProxy{
+    @Override
+    public CurrencyConversion retrieveExchangeValue(String from, String to) {
+        return new CurrencyConversion(0L,from,to,BigDecimal.ZERO,BigDecimal.ZERO,BigDecimal.ZERO,"circuit-breaker fallback");
+        //Implementação mais adequada seria retornar dados em cache de
+        //uma requisição anterior com sucesso
+    }
+}
