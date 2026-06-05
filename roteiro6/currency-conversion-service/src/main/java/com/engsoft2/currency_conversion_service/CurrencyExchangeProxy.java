ac2d3ba46ac15f00cbfd0dbd434a8db3741c9889
Júlio Machado <julioapm@gmail.com>
2026-06-05 13:30:37 -0300

    roteiro6

--- roteiro6/currency-conversion-service/src/main/java/com/engsoft2/currency_conversion_service/CurrencyExchangeProxy.java
+++ roteiro6/currency-conversion-service/src/main/java/com/engsoft2/currency_conversion_service/CurrencyExchangeProxy.java
@@ -1,11 +1,12 @@
-package com.engsoft2.currencyconversionservice;
+package com.engsoft2.currency_conversion_service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange")
+@FeignClient(name = "currency-exchange-service", fallback = CurrencyExchangeFallback.class)
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+
 }
