5736978ea9715fb83df395dec33361a9aa94f14a
Júlio Pereira Machado <julioapm@hotmail.com>
2026-08-14 13:26:27 -0300

    atualizacao spring

--- servicomonolitico/src/test/java/com/exemplo/servicomonolitico/ServicomonoliticoApplicationTests.java
+++ servicomonolitico/src/test/java/com/exemplo/servicomonolitico/ServicomonoliticoApplicationTests.java
@@ -6,7 +6,7 @@ import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
