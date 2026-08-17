e3cac500ece1c45c94da45005811b8c1e29f7a3c
Júlio Pereira Machado <julioapm@hotmail.com>
2026-08-16 22:12:59 -0300

    atualizacao post

--- roteiro0/src/main/java/com/engsoft2/roteiro0/CurrencyConversionController.java
+++ roteiro0/src/main/java/com/engsoft2/roteiro0/CurrencyConversionController.java
@@ -5,9 +5,14 @@ import java.math.BigDecimal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 @RestController
 public class CurrencyConversionController {
@@ -28,6 +33,13 @@ public class CurrencyConversionController {
         currencyExchange.setEnvironment(port);
         return currencyExchange;
     }
+
+    @PostMapping("/currency-exchange")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CurrencyExchange createCurrencyExchange(@RequestBody CurrencyExchange currencyExchange) {
+        logger.info("createCurrencyExchange called with from {} to {}", currencyExchange.getFrom(), currencyExchange.getTo());
+        return repository.save(currencyExchange);
+    }   
 
     private CurrencyExchange getCurrencyExchange(String from, String to) {
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
